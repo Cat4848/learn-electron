c470521535341efb055edd4649ce0b8e981d1a76
Cat4848 <catalinrol@gmail.com>
2024-11-12 05:59:33 +0000

    feat: my first window

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
 console.log("hello from electron ✺");
