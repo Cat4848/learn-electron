bc1d1e985c5ef6194841dfcad0e774c192555fb6
Cat4848 <catalinrol@gmail.com>
2024-11-16 11:42:37 +0000

    feat: added inter process communication

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 const versionsContainer = document.querySelector("#versions");
 versionsContainer.innerText = `Node version: ${window.versions.node()},
   Chrome version: ${window.versions.chrome()}, Electron version: ${window.versions.electron()}`;
+
+const ipcTest = document.querySelector("#ipc-test");
+const serverResponse = async () => {
+  const response = await window.versions.ping();
+  ipcTest.innerHTML = response;
+};
+serverResponse();
